a8371f3b95f02089baa68515ccfd339a6837726d
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2026-03-16 15:14:44 +0530

    modified:   added website name and description 	new file:   created to store the website name and description 	modified:   auto updated 	modified:   auto updated

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Quizhub",
+  description:
+    "An AI-powered quiz and learning platform designed to help students practice smarter, analyze performance, and prepare effectively for school, college, and competitive exams."
+}
