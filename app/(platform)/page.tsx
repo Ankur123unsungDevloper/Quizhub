3bb901e3af1285e2edacc66d047540a87ee14b8c
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2026-04-19 16:00:26 +0530

    modified:   app/(platform)/page.tsx

--- app/(platform)/page.tsx
+++ app/(platform)/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "./navbar/navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-full">
+    <div className="min-h-full md:w-full">
       <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-2 pt-10">
         <Navbar />
         <MainPage />
